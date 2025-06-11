6c85ac18fe5b81be90834288178931c3db6f62c2
Soyab Karki <SoyabKarki123@gmail.com>
2025-06-11 18:26:00 +0545

    File separation for landing page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-import restroImg from '../images/restaurant.jpg';
+import { loadLandingPage } from './landing-page';
 
-const text = document.createElement('div');
-text.className = 'text-center';
-
-const title = document.createElement('h1');
-title.className = 'display-4';
-title.textContent = 'Welcome to Our Restaurant';
-text.appendChild(title);
-
-const paragraph = document.createElement('p');
-paragraph.className = 'lead';
-paragraph.textContent = 'Serving delicious meals with a touch of love.';
-text.appendChild(paragraph);
-
-document.getElementById('content').appendChild(text);
-
-const image = document.createElement('div');
-image.className = 'text-center mt-4 mb-4';
-
-const img = document.createElement('img');
-img.src = restroImg;
-img.className = 'img-fluid rounded shadow';
-image.appendChild(img);
-
-document.getElementById('content').appendChild(image);
+loadLandingPage();
---
+++ src/landing-page.js
@@ -0,0 +1,29 @@
+import restroImg from '../images/restaurant.jpg';
+
+export function loadLandingPage() {
+  const text = document.createElement('div');
+  text.className = 'text-center';
+
+  const title = document.createElement('h1');
+  title.className = 'display-4';
+  title.textContent = 'Welcome to Our Restaurant';
+  text.appendChild(title);
+
+  const paragraph = document.createElement('p');
+  paragraph.className = 'lead';
+  paragraph.textContent = 'Serving delicious meals with a touch of love.';
+  text.appendChild(paragraph);
+
+  const content = document.getElementById('content');
+  content.appendChild(text);
+
+  const image = document.createElement('div');
+  image.className = 'text-center mt-4 mb-4';
+
+  const img = document.createElement('img');
+  img.src = restroImg;
+  img.className = 'img-fluid rounded shadow';
+  image.appendChild(img);
+
+  content.appendChild(image);
+}
